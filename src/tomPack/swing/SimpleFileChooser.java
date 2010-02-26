cb66b1f7999206e616c4eb872f5391b31667eaae
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-02-26 00:40:39 +0000

    Issue 15:  	 Add <File currDir> arg to TomFileChooser and TomDirChooser

--- src/tomPack/swing/SimpleFileChooser.java
+++ src/tomPack/swing/SimpleFileChooser.java
@@ -1,5 +1,7 @@
 package tomPack.swing;
 
+import java.io.File;
+
 import javax.swing.JFileChooser;
 
 public class SimpleFileChooser extends JFileChooser {
@@ -7,7 +9,12 @@ public class SimpleFileChooser extends JFileChooser {
     private static final long serialVersionUID = 5499474442213816724L;
 
     public int showOpenDialog() {
-	return showOpenDialog(null);
+	return super.showOpenDialog(null);
+    }
+    
+    public int showOpenDialog(File currentDirectory) {
+	setCurrentDirectory(currentDirectory);
+	return showOpenDialog();
     }
 
 }
