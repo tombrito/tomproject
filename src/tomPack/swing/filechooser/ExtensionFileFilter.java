91bf4d7df51e83f2e55c3a6f741141d94ab3a0f4
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-02-26 01:01:14 +0000

    Issue 17:  	 Add ExtensionFileFilter

---
+++ src/tomPack/swing/filechooser/ExtensionFileFilter.java
@@ -0,0 +1,28 @@
+package tomPack.swing.filechooser;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+public class ExtensionFileFilter extends FileFilter {
+    
+    private String extension;
+    private String description;
+
+    public ExtensionFileFilter(String extension, String description) {
+	this.extension = extension;
+	this.description = description;
+    }
+
+    @Override
+    public boolean accept(File f) {
+	// TODO f.isDirectory() needed?
+	return f.isDirectory() || f.getName().endsWith(extension);
+    }
+
+    @Override
+    public String getDescription() {
+	return description;
+    }
+    
+}
