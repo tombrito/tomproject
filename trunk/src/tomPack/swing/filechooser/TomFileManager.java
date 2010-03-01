bcb2db3e1a4057f44eb3c67cf7825268f18143d7
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-03-01 22:05:26 +0000

    Issue 29:  	 Create TomFileManager

---
+++ trunk/src/tomPack/swing/filechooser/TomFileManager.java
@@ -0,0 +1,35 @@
+package tomPack.swing.filechooser;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
+import lombok.Data;
+import tomPack.swing.filechooser.ExtensionFileFilter;
+import tomPack.swing.filechooser.SimpleFileChooser;
+
+@Data
+public class TomFileManager {
+
+    private final String ext;
+    private final String description;
+    
+    private File currDir; // current selected directory
+    private File currFile; // current selected file
+
+    public File openOpf() {
+	SimpleFileChooser fc = new SimpleFileChooser();
+	fc.setFileFilter(new ExtensionFileFilter(ext, description));
+
+	fc.setCurrentDirectory(currDir);
+	int result = fc.showOpenDialog();
+	if (result == JFileChooser.APPROVE_OPTION) {
+	    File selected = fc.getSelectedFile();
+	    currFile = selected;
+	    currDir = selected.getParentFile();
+	    return currFile;
+	}
+	return null;
+    }
+
+}
