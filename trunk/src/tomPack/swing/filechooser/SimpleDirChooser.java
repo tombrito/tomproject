d51378a8de0281cefc690541d19d1a605fe4223d
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-03-02 01:47:53 +0000

    enhancement for file manager

--- trunk/src/tomPack/swing/filechooser/SimpleDirChooser.java
+++ trunk/src/tomPack/swing/filechooser/SimpleDirChooser.java
@@ -1,7 +1,7 @@
 package tomPack.swing.filechooser;
 
 
-public class SimpleDirChooser extends SimpleFileChooser {
+public class SimpleDirChooser extends TomFileChooser {
 
     private static final long serialVersionUID = -6191189902994159959L;
 
--- trunk/src/tomPack/swing/filechooser/TomFileManager.java
+++ trunk/src/tomPack/swing/filechooser/TomFileManager.java
@@ -6,33 +6,48 @@ import javax.swing.JFileChooser;
 
 import lombok.Data;
 import tomPack.swing.filechooser.ExtensionFileFilter;
-import tomPack.swing.filechooser.SimpleFileChooser;
+import tomPack.swing.filechooser.TomFileChooser;
 
 @Data
 public class TomFileManager {
 
-    private final String ext;
-    private final String description;
+    private final TomFileChooser fc;
 
     private File currDir; // current selected directory
     private File currFile; // current selected file
 
-    public File showOpenDialog() {
-	SimpleFileChooser fc = new SimpleFileChooser();
+    public TomFileManager(String ext, String description) {
+	fc = new TomFileChooser();
 	fc.setFileFilter(new ExtensionFileFilter(ext, description));
+    }
 
-	fc.setCurrentDirectory(currDir);
-	int result = fc.showOpenDialog();
+    /** @return the selected File to open. */
+    public File showOpenDialog() {
+	int result = fc.showOpenDialog(currDir);
 	if (result == JFileChooser.APPROVE_OPTION) {
 	    File selected = fc.getSelectedFile();
-	    currFile = selected;
-	    currDir = selected.getParentFile();
-	    return currFile;
+	    if (selected != null) {
+		currFile = selected;
+		currDir = selected.getParentFile();
+		return currFile;
+	    }
 	}
 	return null;
     }
 
-    // TODO showSaveAsDialog, ask if override file, return boolean to save or
-    // not
+    /** @return the selected File to write on. */
+    public File showSaveDialog() {
+	int result = fc.showSaveDialog(currDir);
+	if (result == JFileChooser.APPROVE_OPTION) {
+	    // TODO check if is overriding existing file
+	    File selected = fc.getSelectedFile();
+	    if (selected != null) {
+		currFile = selected;
+		currDir = selected.getParentFile();
+		return currFile;
+	    }
+	}
+	return null;
+    }
 
 }
