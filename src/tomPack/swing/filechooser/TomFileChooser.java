e92daf91456b974eb17e4d73684e36d40bd85e2e
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-03-07 18:04:22 +0000

    Issue 42:	TomFileChooser must be package acessible

--- src/tomPack/swing/filechooser/TomFileChooser.java
+++ src/tomPack/swing/filechooser/TomFileChooser.java
@@ -4,26 +4,29 @@ import java.io.File;
 
 import javax.swing.JFileChooser;
 
-public class TomFileChooser extends JFileChooser {
-    
+/**
+ * Used by {@link TomFileManager} to manage files.
+ */
+class TomFileChooser extends JFileChooser {
+
     private static final long serialVersionUID = 5499474442213816724L;
 
     public int showOpenDialog() {
 	return super.showOpenDialog(null);
     }
-    
+
     public int showOpenDialog(File currentDirectory) {
 	setCurrentDirectory(currentDirectory);
 	return showOpenDialog();
     }
 
     public int showSaveDialog() {
-        return super.showSaveDialog(null);
+	return super.showSaveDialog(null);
     }
 
     public int showSaveDialog(File currentDirectory) {
 	setCurrentDirectory(currentDirectory);
-        return super.showSaveDialog(null);
+	return super.showSaveDialog(null);
     }
 
 }
