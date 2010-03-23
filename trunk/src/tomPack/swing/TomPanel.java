5f2c9918dc417a18699021bc89526b8b32a95e5d
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-03-23 06:18:45 +0000

    moving TomPanel layout methods to TomEasyLayoutPanel

--- trunk/src/tomPack/swing/TomPanel.java
+++ trunk/src/tomPack/swing/TomPanel.java
@@ -1,25 +1,15 @@
 package tomPack.swing;
 
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
 import java.awt.Component;
-import java.awt.Container;
 import java.awt.Cursor;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.PopupMenu;
 
-import javax.swing.BoxLayout;
-import javax.swing.GroupLayout;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SpringLayout;
 
-public class TomPanel extends JPanel {
+public class TomPanel extends TomEasyLayoutPanel {
 
     //
     // Constructors
@@ -32,100 +22,6 @@ public class TomPanel extends JPanel {
     public TomPanel(LayoutManager mgr) {
 	this();
 	setLayout(mgr);
-    }
-
-    //
-    // Layout methods
-    //
-
-    public LayoutManager setBorderLayout() {
-	LayoutManager layout = new BorderLayout();
-	setLayout(layout);
-	return layout;
-    }
-
-    public LayoutManager setBorderLayout(int hgap, int vgap) {
-	LayoutManager layout = new BorderLayout(hgap, vgap);
-	setLayout(layout);
-	return layout;
-    }
-
-    public LayoutManager setBoxLayoutX() {
-	LayoutManager layout = new BoxLayout(this, BoxLayout.X_AXIS);
-	setLayout(layout);
-	return layout;
-    }
-
-    public LayoutManager setBoxLayoutY() {
-	LayoutManager layout = new BoxLayout(this, BoxLayout.Y_AXIS);
-	setLayout(layout);
-	return layout;
-    }
-
-    public LayoutManager setCardLayout() {
-	LayoutManager layout = new CardLayout();
-	setLayout(layout);
-	return layout;
-    }
-
-    public LayoutManager setCardLayout(int hgap, int vgap) {
-	LayoutManager layout = new CardLayout(hgap, vgap);
-	setLayout(layout);
-	return layout;
-    }
-
-    public LayoutManager setFlowLayout() {
-	LayoutManager layout = new FlowLayout();
-	setLayout(layout);
-	return layout;
-    }
-
-    public LayoutManager setFlowLayout(int align) {
-	LayoutManager layout = new FlowLayout(align);
-	setLayout(layout);
-	return layout;
-    }
-
-    public LayoutManager setFlowLayout(int align, int hgap, int vgap) {
-	LayoutManager layout = new FlowLayout(align, hgap, vgap);
-	setLayout(layout);
-	return layout;
-    }
-
-    public LayoutManager setGridBagLayout() {
-	LayoutManager layout = new GridBagLayout();
-	setLayout(layout);
-	return layout;
-    }
-
-    public LayoutManager setGridLayout() {
-	LayoutManager layout = new GridLayout();
-	setLayout(layout);
-	return layout;
-    }
-
-    public LayoutManager setGridLayout(int rows, int cols) {
-	LayoutManager layout = new GridLayout(rows, cols);
-	setLayout(layout);
-	return layout;
-    }
-
-    public LayoutManager setGridLayout(int rows, int cols, int hgap, int vgap) {
-	LayoutManager layout = new GridLayout(rows, cols, hgap, vgap);
-	setLayout(layout);
-	return layout;
-    }
-
-    public LayoutManager setGroupLayout(Container host) {
-	LayoutManager layout = new GroupLayout(host);
-	setLayout(layout);
-	return layout;
-    }
-
-    public LayoutManager setSpringLayout() {
-	LayoutManager layout = new SpringLayout();
-	setLayout(layout);
-	return layout;
     }
 
     //
