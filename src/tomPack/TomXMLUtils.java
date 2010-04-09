a51cbc9d70ada4e8ce237f950f389e535c6b6444
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-04-09 01:18:15 +0000

    Issue 72:	writeDomToStream

--- src/tomPack/TomXMLUtils.java
+++ src/tomPack/TomXMLUtils.java
@@ -2,12 +2,22 @@ package tomPack;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -36,8 +46,24 @@ public class TomXMLUtils {
 	DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
 	// TODO show progress bar
 	// TODO assess use SAX (research)
-	System.out.println("parsing...");
+	System.out.println("parsing..."); //$NON-NLS-1$
 	return docBuilder.parse(xmlFile);
+    }
+
+    // This method writes a DOM document to a stream
+    public static void outputDOM(Document doc, PrintStream stream) throws TransformerFactoryConfigurationError,
+	    TransformerException {
+
+	// Prepare the DOM document for writing
+	Source source = new DOMSource(doc);
+	// Prepare the output stream
+	Result result = new StreamResult(stream);
+
+	// Write the DOM document to the file
+	Transformer xformer = TransformerFactory.newInstance().newTransformer();
+	xformer.setOutputProperty(OutputKeys.INDENT, "yes"); //$NON-NLS-1$
+	xformer.transform(source, result);
+
     }
 
 }
