e78835822df93dd2c4faff7a88818b69f35df071
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-04-09 01:06:10 +0000

    Issue 73:	TomXMLUtils.parseXML

--- trunk/src/tomPack/TomXMLUtils.java
+++ trunk/src/tomPack/TomXMLUtils.java
@@ -1,14 +1,22 @@
 package tomPack;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class TomXMLUtils {
-    
+
     public static List<String> getValueList(Element rootElement) {
 	List<String> valueList = new ArrayList<String>();
 	NodeList manifestNodes = rootElement.getChildNodes();
@@ -21,6 +29,15 @@ public class TomXMLUtils {
 	    valueList.add(nodeName);
 	}
 	return valueList;
+    }
+
+    public static Document parseXML(File xmlFile) throws ParserConfigurationException, SAXException, IOException {
+	DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+	// TODO show progress bar
+	// TODO assess use SAX (research)
+	System.out.println("parsing...");
+	return docBuilder.parse(xmlFile);
     }
 
 }
