22e0809456673cd0f08201038262c5946c02a28d
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-04-15 19:31:51 +0000

    hexToBytes upgrade

--- trunk/src/tomPack/TomHexUtils.java
+++ trunk/src/tomPack/TomHexUtils.java
@@ -1,29 +1,14 @@
 package tomPack;
 
+import java.math.BigInteger;
+
 public class TomHexUtils {
 
     /**
-     * http://www.java2s.com/Code/Java/Development- Class/ConverthexToBytes.htm
+     * Convenience method for "new BigInteger(hexString, 16).toByteArray()".
      */
-    public static byte[] hexToBytes(char[] hex) {
-	int length = hex.length / 2;
-	byte[] raw = new byte[length];
-	for (int i = 0; i < length; i++) {
-	    int high = Character.digit(hex[i * 2], 16);
-	    int low = Character.digit(hex[i * 2 + 1], 16);
-	    int value = (high << 4) | low;
-	    if (value > 127)
-		value -= 256;
-	    raw[i] = (byte) value;
-	}
-	return raw;
-    }
-
-    /**
-     * http://www.java2s.com/Code/Java/Development- Class/ConverthexToBytes.htm
-     */
-    public static byte[] hexToBytes(String hex) {
-	return hexToBytes(hex.toCharArray());
+    public static byte[] hexToBytes(String hexString) {
+	return new BigInteger(hexString, 16).toByteArray();
     }
 
 }
