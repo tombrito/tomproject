aaba2552a699d3bb80cf560fd43724f49a8aa1de
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-04-20 20:29:34 +0000

    Creating TomCalendar

---
+++ src/tomPack/TomCalendar.java
@@ -0,0 +1,53 @@
+package tomPack;
+
+import java.util.Calendar;
+
+public class TomCalendar {
+
+    @SuppressWarnings("nls")
+    public static String getTime() {
+	String time = "";
+	time += getYear();
+	time += "-" + getMonth();
+	time += "-" + getDay();
+	time += "-" + getHour();
+	time += "-" + getMinute();
+	time += "-" + getSecond();
+	time += "-" + getMillisecond();
+	System.out.println(time);
+	return time;
+    }
+
+    public static int getYear() {
+	return Calendar.getInstance().get(Calendar.YEAR);
+    }
+
+    public static int getMonth() {
+	return Calendar.getInstance().get(Calendar.MONTH + 1);
+    }
+
+    public static int getDay() {
+	return Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+    }
+
+    public static int getHour() {
+	return Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+    }
+
+    public static int getMinute() {
+	return Calendar.getInstance().get(Calendar.MINUTE);
+    }
+
+    public static int getSecond() {
+	return Calendar.getInstance().get(Calendar.SECOND);
+    }
+
+    public static int getMillisecond() {
+	return Calendar.getInstance().get(Calendar.MILLISECOND);
+    }
+    
+    public static void main(String[] args) {
+	TomCalendar.getTime();
+    }
+
+}
