5bdc64ac79ac65646baad073e3910f6cf8834969
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-04-22 17:43:17 +0000

    TomCalendar update. Completing date values with "0"s to make a correct alphabetic ordination.

--- trunk/src/tomPack/TomCalendar.java
+++ trunk/src/tomPack/TomCalendar.java
@@ -4,17 +4,62 @@ import java.util.Calendar;
 
 public class TomCalendar {
 
-    @SuppressWarnings("nls")
+    public static String getDateAndTime() {
+	StringBuilder time = new StringBuilder();
+	time.append(getDate());
+	time.append("_"); //$NON-NLS-1$
+	time.append(getTimeMilliseconds());
+	return time.toString();
+    }
+
+    @SuppressWarnings("boxing")
     public static String getTime() {
-	String time = "";
-	time += getYear();
-	time += "-" + getMonth();
-	time += "-" + getDay();
-	time += "-" + getHour();
-	time += "-" + getMinute();
-	time += "-" + getSecond();
-	time += "-" + getMillisecond();
-	return time;
+
+	int hour = getHour();
+	int minute = getMinute();
+	int second = getSecond();
+
+	StringBuilder time = new StringBuilder();
+	time.append((hour <= 9) ? ("0" + hour) : hour); //$NON-NLS-1$
+	time.append("-"); //$NON-NLS-1$
+	time.append((minute <= 9) ? ("0" + minute) : minute); //$NON-NLS-1$
+	time.append("-"); //$NON-NLS-1$
+	time.append((second <= 9) ? ("0" + second) : second); //$NON-NLS-1$
+
+	return time.toString();
+    }
+
+    public static String getTimeMilliseconds() {
+
+	StringBuilder millis = new StringBuilder();
+	millis.append(getMillisecond());
+	while (millis.length() < 3) {
+	    millis.insert(0, "0"); //$NON-NLS-1$
+	}
+
+	StringBuilder timeMillis = new StringBuilder();
+	timeMillis.append(getTime());
+	timeMillis.append("_"); //$NON-NLS-1$
+	timeMillis.append((millis));
+
+	return timeMillis.toString();
+    }
+
+    @SuppressWarnings("boxing")
+    public static String getDate() {
+
+	StringBuilder date = new StringBuilder();
+	int year = getYear();
+	int month = getMonth();
+	int day = getDay();
+
+	date.append(year);
+	date.append("-"); //$NON-NLS-1$
+	date.append((month <= 9) ? ("0" + month) : month); //$NON-NLS-1$
+	date.append("-"); //$NON-NLS-1$
+	date.append((day <= 9) ? ("0" + day) : day); //$NON-NLS-1$
+
+	return date.toString();
     }
 
     public static int getYear() {
@@ -22,7 +67,7 @@ public class TomCalendar {
     }
 
     public static int getMonth() {
-	return Calendar.getInstance().get(Calendar.MONTH + 1);
+	return (Calendar.getInstance().get(Calendar.MONTH) + 1);
     }
 
     public static int getDay() {
@@ -43,6 +88,10 @@ public class TomCalendar {
 
     public static int getMillisecond() {
 	return Calendar.getInstance().get(Calendar.MILLISECOND);
+    }
+
+    public static void main(String[] args) {
+	System.out.println(TomCalendar.getDateAndTime());
     }
 
 }
