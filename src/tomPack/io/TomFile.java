66858c3ddc54d310e0d3df31cda8be408286fbac
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-04-22 20:09:14 +0000

    creating TomFile

---
+++ src/tomPack/io/TomFile.java
@@ -0,0 +1,47 @@
+package tomPack.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+
+public class TomFile extends File {
+    
+    public TomFile(String pathname) {
+	super(pathname);
+    }
+
+    public TomFile(String parent, String child) {
+	super(parent, child);
+    }
+    
+    public TomFile(File parent, String child) {
+	super(parent, child);
+    }
+    
+    public TomFile(URI uri) {
+	super(uri);
+    }
+    
+    //
+    //
+    //
+
+    /**
+     * Create the path, if it does not exist.
+     */
+    public void createPath() throws IOException {
+	if (!exists() && !mkdirs()) {
+	    throw new IOException("Fail to create path: " + this); //$NON-NLS-1$
+	}
+    }
+    
+    /**
+     * Create the file, if it does not exist.
+     */
+    public void createFile() throws IOException {
+	if (!exists() && !createNewFile()) {
+	    throw new IOException("Fail to create file: " + this); //$NON-NLS-1$
+	}
+    }
+    
+}
