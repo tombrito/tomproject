fcd6d98cbd15130f31803bc5447d336cb4c4f86a
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-05-04 16:49:31 +0000

    TomIOUtils refactoring

--- trunk/src/tomPack/io/TomIOUtils.java
+++ trunk/src/tomPack/io/TomIOUtils.java
@@ -6,12 +6,13 @@ import java.io.IOException;
 public class TomIOUtils {
 
     /**
-     * Try to close the Closable object, and print the stack trace if an exception is raised.
+     * Try to close the Closable object, and print the stack trace if an
+     * exception is raised.
+     * 
+     * Useful to close a set of streams without throw an exception.
      */
     public static void close(Closeable c) {
-	if (c == null)
-	    return;
-	try {
+	if (c != null) try {
 	    c.close();
 	} catch (IOException e) {
 	    // log the exception (if you are using System.out as log)
