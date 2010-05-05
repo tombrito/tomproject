4584494361fb6837168e84dfa9feeff9961d895a
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-05-05 13:55:26 +0000

    TomPanel enhancements.

--- src/tomPack/swing/TomPanel.java
+++ src/tomPack/swing/TomPanel.java
@@ -6,6 +6,9 @@ import java.awt.Dimension;
 import java.awt.LayoutManager;
 import java.awt.PopupMenu;
 
+import javax.swing.Action;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 
@@ -25,13 +28,39 @@ public class TomPanel extends EasyLayoutPanel {
     }
 
     //
+    // Border
+    //
+
+    public void setEmptyBorder() {
+	setBorder(BorderFactory.createEmptyBorder());
+    }
+
+    public void setEmptyBorder(int top, int left, int bottom, int right) {
+	setBorder(BorderFactory.createEmptyBorder(top, left, bottom, right));
+    }
+
+    //
     // Other methods
     //
 
+    /**
+     * If the object can be converted to a {@link Component}, convert and add.<br>
+     * 
+     * <ul>
+     * <li> {@link Component} is added normally.
+     * <li> {@link String} is converted into a {@link JLabel}.
+     * <li> {@link Action} is converted into a {@link JButton}.
+     * </ul>
+     * 
+     * @param objs
+     *            - objects to be converted and added.
+     */
     public void add(Object... objs) {
 	for (Object obj : objs) {
 	    if (obj instanceof String) {
 		add(new JLabel((String) obj));
+	    } else if (obj instanceof Action) {
+		add(new JButton((Action) obj));
 	    } else if (obj instanceof PopupMenu) {
 		super.add((PopupMenu) obj);
 	    } else if (obj instanceof Component) {
