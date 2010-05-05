cd6cb04e8120206b35d88ca6417955d31018863d
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-05-05 20:10:09 +0000

    Introducing VerticalPanel

---
+++ trunk/src/tomPack/swing/panel/VerticalPanel.java
@@ -0,0 +1,36 @@
+package tomPack.swing.panel;
+
+import java.awt.LayoutManager;
+
+import tomPack.swing.TomPanel;
+
+public class VerticalPanel extends TomPanel {
+
+    //
+    // Constructors
+    //
+
+    public VerticalPanel() {
+	super();
+	init();
+    }
+
+    public VerticalPanel(boolean isDoubleBuffered) {
+	super(isDoubleBuffered);
+	init();
+    }
+
+    public VerticalPanel(LayoutManager layout, boolean isDoubleBuffered) {
+	super(layout, isDoubleBuffered);
+	init();
+    }
+
+    //
+    // 
+    //
+
+    private void init() {
+	setBoxLayoutY();
+    }
+
+}
