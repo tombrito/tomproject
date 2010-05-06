6f2a11729f52e99d2a8d8eb2d417b42537a295b1
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-05-06 12:21:21 +0000

    layoutDefinedPanels refactoring

---
+++ src/tomPack/swing/layoutDefinedPanels/VerticalPanel.java
@@ -0,0 +1,36 @@
+package tomPack.swing.layoutDefinedPanels;
+
+import java.awt.LayoutManager;
+
+import tomPack.swing.TomPanel;
+
+public class VerticalPanel extends TomPanel {
+
+    //
+    // Constructors
+    //
+
+    public VerticalPanel() {
+	super();
+	init();
+    }
+
+    public VerticalPanel(boolean isDoubleBuffered) {
+	super(isDoubleBuffered);
+	init();
+    }
+
+    public VerticalPanel(LayoutManager layout, boolean isDoubleBuffered) {
+	super(layout, isDoubleBuffered);
+	init();
+    }
+
+    //
+    // 
+    //
+
+    private void init() {
+	setBoxLayoutY();
+    }
+
+}
