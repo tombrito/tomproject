23a9d4bcd386f027bed3f484d13c168858f0b822
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-05-20 19:56:03 +0000

    new method: TomRsaUtils.generateKeyPair()

--- src/tomPack/crypto/TomRsaUtils.java
+++ src/tomPack/crypto/TomRsaUtils.java
@@ -2,6 +2,10 @@ package tomPack.crypto;
 
 import java.io.Serializable;
 import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 
 import javax.crypto.Cipher;
 
@@ -9,7 +13,7 @@ import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang.SerializationUtils;
 
 public class TomRsaUtils {
-    
+
     public static String encryptAsString(Serializable data, Key key) throws RsaException {
 	return encryptAsString(SerializationUtils.serialize(data), key);
     }
@@ -25,13 +29,25 @@ public class TomRsaUtils {
 
     public static byte[] encryptAsByteArray(byte[] bytes, Key key) throws RsaException {
 	try {
-	Cipher cipher = Cipher.getInstance("RSA/None/NoPadding", "BC"); //$NON-NLS-1$ //$NON-NLS-2$
-	cipher.init(Cipher.ENCRYPT_MODE, key);
-	byte[] encryptedBytes = cipher.doFinal(bytes);
-	return encryptedBytes;
+	    Cipher cipher = Cipher.getInstance("RSA/None/NoPadding", "BC"); //$NON-NLS-1$ //$NON-NLS-2$
+	    cipher.init(Cipher.ENCRYPT_MODE, key);
+	    byte[] encryptedBytes = cipher.doFinal(bytes);
+	    return encryptedBytes;
 	} catch (Exception e) {
 	    throw new RsaException(e);
 	}
     }
-    
+
+    /**
+     * Convenience method for:
+     * 
+     * <pre>
+     * java.security.KeyPairGenerator.getInstance(&quot;RSA&quot;, &quot;BC&quot;).generateKeyPair()
+     * </pre>
+     */
+    @SuppressWarnings("nls")
+    public static KeyPair generateKeyPair() throws NoSuchAlgorithmException, NoSuchProviderException {
+	return KeyPairGenerator.getInstance("RSA", "BC").generateKeyPair();
+    }
+
 }
