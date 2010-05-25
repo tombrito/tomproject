9dd8eaf60a5668ec4af9e9e75f4fc73ac7a7be26
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-05-25 18:27:44 +0000

    TomAesUtils encrypt/decrypt

---
+++ src/tomPack/crypto/AesDecryptionException.java
@@ -0,0 +1,11 @@
+package tomPack.crypto;
+
+public class AesDecryptionException extends Exception {
+
+    public AesDecryptionException() {}
+
+    public AesDecryptionException(Exception e) {
+	super(e);
+    }
+
+}
---
+++ src/tomPack/crypto/AesEncryptionException.java
@@ -0,0 +1,11 @@
+package tomPack.crypto;
+
+public class AesEncryptionException extends Exception {
+
+    public AesEncryptionException() {}
+
+    public AesEncryptionException(Exception e) {
+	super(e);
+    }
+
+}
