7d56286b041f1d9656eb76b2c245c425f93b0ccf
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-05-25 18:27:44 +0000

    TomAesUtils encrypt/decrypt

--- trunk/src/tomPack/crypto/TomAesUtils.java
+++ trunk/src/tomPack/crypto/TomAesUtils.java
@@ -1,14 +1,21 @@
 package tomPack.crypto;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.SecretKeySpec;
 
 import tomPack.TomHexUtils;
+import tomPack.io.TomIOUtils;
 
 @SuppressWarnings("nls")
 public class TomAesUtils {
@@ -16,7 +23,7 @@ public class TomAesUtils {
     public static SecretKey createAesKey(String encoded) throws NoSuchAlgorithmException, InvalidKeySpecException {
 	return createAesKey(TomHexUtils.toHexBytes(encoded));
     }
-    
+
     public static SecretKey createAesKey(byte[] encoded) throws NoSuchAlgorithmException, InvalidKeySpecException {
 	SecretKeySpec keySpec = new SecretKeySpec(encoded, "AES");
 	SecretKeyFactory factory = SecretKeyFactory.getInstance("AES");
@@ -26,6 +33,54 @@ public class TomAesUtils {
     public static SecretKey generateAesKey() throws NoSuchAlgorithmException {
 	KeyGenerator keygen = KeyGenerator.getInstance("AES");
 	return keygen.generateKey();
+    }
+
+    public static void encrypt(Key key, InputStream in, OutputStream out) throws AesEncryptionException {
+
+	// Buffer used to transport the bytes from one stream to another
+	byte[] buf = new byte[1024];
+
+	try {
+	    Cipher ecipher = Cipher.getInstance("AES/CBC/NoPadding");
+	    ecipher.init(Cipher.ENCRYPT_MODE, key);
+
+	    // Bytes written to out will be encrypted
+	    out = new CipherOutputStream(out, ecipher);
+
+	    // Read in the cleartext bytes and write to out to encrypt
+	    int numRead = 0;
+	    while ((numRead = in.read(buf)) >= 0) {
+		out.write(buf, 0, numRead);
+	    }
+	    // FIXME must be closed by caller
+	    TomIOUtils.close(out);
+	} catch (Exception e) {
+	    throw new AesEncryptionException(e);
+	}
+    }
+
+    public static void decrypt(Key key, InputStream in, OutputStream out) throws AesDecryptionException {
+
+	// Buffer used to transport the bytes from one stream to another
+	byte[] buf = new byte[1024];
+
+	try {
+	    Cipher dcipher = Cipher.getInstance("AES/CBC/NoPadding");
+	    dcipher.init(Cipher.DECRYPT_MODE, key);
+
+	    // Bytes read from in will be decrypted
+	    in = new CipherInputStream(in, dcipher);
+
+	    // Read in the decrypted bytes and write the cleartext to out
+	    int numRead = 0;
+	    while ((numRead = in.read(buf)) >= 0) {
+		out.write(buf, 0, numRead);
+	    }
+	    // FIXME must be closed by caller
+	    out.close();
+	} catch (Exception e) {
+	    throw new AesDecryptionException(e);
+	}
     }
 
 }
