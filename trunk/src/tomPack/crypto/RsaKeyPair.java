14b58ff7dbbb9b87231c0ca14640cad3f9434e17
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-06-01 19:55:28 +0000

    Introducing RsaKeyPair

---
+++ trunk/src/tomPack/crypto/RsaKeyPair.java
@@ -0,0 +1,50 @@
+package tomPack.crypto;
+
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+/**
+ * As {@link KeyPair} is final and cannot be extended, this class composes and
+ * delegates {@link KeyPair} methods using RSA keys.
+ */
+public class RsaKeyPair {
+
+    private KeyPair keyPair;
+
+    public RsaKeyPair(KeyPair keyPair) {
+	this.keyPair = keyPair;
+    }
+
+    //
+    // KeyPair delegations
+    //
+
+    @Override
+    public int hashCode() {
+	return keyPair.hashCode();
+    }
+
+    @Override
+    public String toString() {
+	return keyPair.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	return keyPair.equals(obj);
+    }
+
+    public RSAPrivateKey getPrivate() {
+	return (RSAPrivateKey) keyPair.getPrivate();
+    }
+
+    public RSAPublicKey getPublic() {
+	return (RSAPublicKey) keyPair.getPublic();
+    }
+
+    //
+    // KeyPair delegations
+    //
+
+}
