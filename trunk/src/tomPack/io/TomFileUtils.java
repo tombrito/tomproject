71b80f4a513d18cba85e9dc388092ccecb15b24f
brito.pro <brito.pro@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-06-08 13:36:49 +0000

    TomFileUtils copy methods

--- trunk/src/tomPack/io/TomFileUtils.java
+++ trunk/src/tomPack/io/TomFileUtils.java
@@ -1,7 +1,15 @@
 package tomPack.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
 
 /**
  * Utilities for {@link File} handling.
@@ -63,4 +71,33 @@ public class TomFileUtils {
 	}
     }
 
+    public static void copyFile(File inputFile, File targetDir) throws IOException {
+	copyFile(inputFile, targetDir, inputFile.getName());
+    }
+
+    public static void copyFile(File inputFile, File targetDir, String newName) throws IOException {
+	InputStream in = new FileInputStream(inputFile);
+	OutputStream out = new FileOutputStream(new File(targetDir, newName));
+	IOUtils.copy(in, out);
+    }
+
+    public static void copyFiles(List<File> files, File destDir) throws IOException {
+	for (File file : files) {
+	    copyFile(file, destDir);
+	}
+    }
+
+    /**
+     * Its common for {@link TomFile} to use {@link TomFileUtils}, not the
+     * opposite. But this is an exception, as the List of files can not be just
+     * casted to List of TomFiles.
+     */
+    public static void copyFiles(List<TomFile> files, TomFile destDir) throws IOException {
+	List<File> newFiles = new ArrayList<File>();
+	for (File file : files) {
+	    newFiles.add(file);
+	}
+	copyFiles(newFiles, destDir);
+    }
+    
 }
