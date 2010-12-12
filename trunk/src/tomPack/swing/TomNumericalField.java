2e4badd358ff32159804a8156ab0e24c593287a5
brito.pro@gmail.com <brito.pro@gmail.com@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-12-12 19:56:47 +0000

    TomNumericalField formatting

--- trunk/src/tomPack/swing/TomNumericalField.java
+++ trunk/src/tomPack/swing/TomNumericalField.java
@@ -2,33 +2,34 @@ package tomPack.swing;
 
 public class TomNumericalField extends TomTextField {
 
-    private static final long serialVersionUID = -5345336767099372835L;
+	private static final long serialVersionUID = -5345336767099372835L;
 
-    public TomNumericalField() {
-	setNumbersOnly(true);
-    }
-
-    @Override
-    protected void setNumbersOnly(boolean numbersOnly) {
-	if (!numbersOnly) {
-	    throw new IllegalArgumentException("Trying to make a numerical field non-numerical."); //$NON-NLS-1$
+	public TomNumericalField() {
+		setNumbersOnly(true);
 	}
-    }
 
-    public int getValue() {
-	String text = getText();
-	return (text.isEmpty()) ? 0 : Integer.valueOf(text).intValue();
-    }
+	@Override
+	protected void setNumbersOnly(boolean numbersOnly) {
+		if (!numbersOnly) {
+			String msg = "Trying to make a numerical field non-numerical."; //$NON-NLS-1$
+			throw new IllegalArgumentException(msg);
+		}
+	}
 
-    // TODO Issue #88: create TomNumericalField.setMaxValue method
+	public int getValue() {
+		String text = getText();
+		return (text.isEmpty()) ? 0 : Integer.valueOf(text).intValue();
+	}
 
-    public static void main(String[] args) {
-	// FIXME Issue 104: TomNumericalField - "numbers only" is not working
-	TomMainFrame f = new TomMainFrame();
-	f.setSize(200, 200);
-	f.restore();
-	f.getContentPane().add(new TomNumericalField());
-	f.setVisible(true);
-    }
+	// TODO Issue #88: create TomNumericalField.setMaxValue method
+
+	public static void main(String[] args) {
+		// FIXME Issue 104: TomNumericalField - "numbers only" is not working
+		TomMainFrame f = new TomMainFrame();
+		f.setSize(200, 200);
+		f.restore();
+		f.getContentPane().add(new TomNumericalField());
+		f.setVisible(true);
+	}
 
 }
