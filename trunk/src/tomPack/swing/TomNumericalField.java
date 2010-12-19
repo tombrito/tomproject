c67081dc89cda5d1effb27d71997e4fc42df0197
brito.pro@gmail.com <brito.pro@gmail.com@50d1f170-dab1-11de-971a-1339e0424b6a>
2010-12-19 08:19:32 +0000

    Fixed Issue 104 (by jhonystein)

--- trunk/src/tomPack/swing/TomNumericalField.java
+++ trunk/src/tomPack/swing/TomNumericalField.java
@@ -5,7 +5,7 @@ public class TomNumericalField extends TomTextField {
 	private static final long serialVersionUID = -5345336767099372835L;
 
 	public TomNumericalField() {
-		setNumbersOnly(true);
+		super.setNumbersOnly(true);
 	}
 
 	@Override
@@ -24,7 +24,6 @@ public class TomNumericalField extends TomTextField {
 	// TODO Issue #88: create TomNumericalField.setMaxValue method
 
 	public static void main(String[] args) {
-		// FIXME Issue 104: TomNumericalField - "numbers only" is not working
 		TomMainFrame f = new TomMainFrame();
 		f.setSize(200, 200);
 		f.restore();
